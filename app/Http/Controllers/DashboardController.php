f4decca789b4b4e811ce370ad11e4a48369d3c7f
Zathura160801 <adipz160801@gmail.com>
2025-09-04 19:01:58 +0800

    membuat roomservice dan mengambil data rooms

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Services\RoomService;
 
 class DashboardController extends Controller
 {
-    public function index(): Response
+    public function index(RoomService $roomService): Response
     {
+        $rooms = $roomService->getRooms();
+
         return Inertia::render('Index');
     }
 }
